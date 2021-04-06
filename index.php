0198391fb452316501f6ba37702b6bb3e0c59bc5
BrianBNguyen <59449776+BrianBNguyen@users.noreply.github.com>
2021-04-06 19:33:55 -0400

    Login page

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
+<?php
+session_start();
+
+$mysql_connect = mysqli_connect("localhost", "id16543739_cdbat", "Cypress@12345", "id16543739_cypress");
+
+// check if database connected properly
+if(!$mysql_connect){
+    die(mysqli_connect_error());
+}
+// else{
+//     print("connected");
+// }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+<?php
+include 'header.php';
+?>
 <head>
     <meta charset="UTF-8">
     <title>CPS406 Cypress</title>
